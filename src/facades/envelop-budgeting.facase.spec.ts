6eeaf2a9d41f8df2b301d408d131bd7d36e064f0
Sarawut Positwinyu <sarawut.positwinyu@gmail.com>
2018-05-29 17:07:27 +0700

    adjust unit test

---
+++ src/facades/envelop-budgeting.facase.spec.ts
@@ -0,0 +1,18 @@
+import EnvelopBudgetingFacade from './envelop-budgeting.facade';
+import Asset from '../domain-model/asset.domain-model';
+
+describe('EnvelopBudgetingFacade', () => {
+
+    it('when getAvaliableBudget can get value correctly',()=>{
+        //Arrange
+        const facade = new EnvelopBudgetingFacade();
+        const assets = <Asset[]>[<Asset>{name:'wallet',value:2000},
+                                 <Asset>{name:'kbank',value:3000}];
+
+        //Act                        
+        const result = facade.getAvaliableBudget(assets)
+
+        //Assert
+        expect(result).toBe(5000);
+    })
+});
